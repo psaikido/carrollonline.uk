73a8746c23290fb8f128d84bbcf9c9468d61efd6
Hugh Carroll <hughie@carrollonline.co.uk>
2023-09-30 15:14:14 +0100

    Publishable!

---
+++ js/toggler.js
@@ -0,0 +1,15 @@
+$('.island-hugle').hover(function () {
+	$('.island-desc-hugle').toggle();
+})
+
+$('.island-mantrasphere').hover(function () {
+	$('.island-desc-mantrasphere').toggle();
+})
+
+$('.island-archive').hover(function () {
+	$('.island-desc-archive').toggle();
+})
+
+$('.island-chan').hover(function () {
+	$('.island-desc-chan').toggle();
+})
