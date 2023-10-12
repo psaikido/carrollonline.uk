dc4e304b86ce8b0b6541c091e82fee8d3af683b5
Hugh Carroll <hughie@carrollonline.co.uk>
2023-10-12 22:31:05 +0100

    Add some gsap animations.

--- js/gsap/animations.js
+++ js/gsap/animations.js
@@ -2,11 +2,29 @@ function modeSwitcher() {
 	document.body.classList.toggle('dark-mode');
 }
 
-let timeline = gsap.timeline({
-	defaults: {
-		ease: "power4.inOut",
-		duration: 2
-	}
-});
+// let timeline = gsap.timeline({
+// 	defaults: {
+// 		ease: "Power0.inOut",
+// 		duration: 1
+// 	}
+// });
+//
+// timeline.from(
+// 	'section h1', 1.5, {x: '-150px', opacity: 0, ease: 'Elastic.easeOut'}
+// );
+// timeline.from(
+// 	'section ul', 1.5, {x: '-150px', opacity: 0, ease: 'Elastic.easeOut'}
+// );
+// timeline.from(
+// 	'section img', 2, {opacity: 0, ease: 'Power0.easeIn'}
+// );
 
-timeline.from('section h1', {x: '5000px'});
+TweenMax.from(
+ 	'section h1', 1.5, {x: '-150px', opacity: 0, ease: 'Elastic.easeOut'}
+);
+TweenMax.from(
+	'section ul', 1.5, {x: '-150px', opacity: 0, ease: 'Elastic.easeOut', delay: .5}
+);
+TweenMax.from(
+	'section img', 2, {opacity: 0, ease: 'Power0.easeIn', delay: 1}
+);
